567e27e5c0f2aff4e4a0fe8ba79d31cebe1d2026
Eduardo Gimenez <24706265+Hadaward@users.noreply.github.com>
2023-12-29 17:14:16 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const crypto = require('crypto');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'files');
+
+function calculateMD5(filePath) {
+  const fileContent = fs.readFileSync(filePath);
+  return crypto.createHash('md5').update(fileContent).digest('hex');
+}
+
+function generateFileHashes() {
+  const fileHashes = {};
+
+  // Read files from the specified folder
+  const files = fs.readdirSync(folderPath);
+
+  // Generate hashes for each file
+  files.forEach((file) => {
+    const filePath = path.join(folderPath, file);
+    const hash = calculateMD5(filePath);
+    fileHashes[file] = hash;
+  });
+
+  // Write the hashes to a JSON file
+  const outputFilePath = 'hash.json';
+  fs.writeFileSync(outputFilePath, JSON.stringify(fileHashes, null, 2));
+
+  console.log(`File hashes generated and saved to ${outputFilePath}`);
+}
+
+generateFileHashes();
