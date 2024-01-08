a4308caad3314f8e64643d0fa6ceea1b22298dab
Eduardo Gimenez <24706265+Hadaward@users.noreply.github.com>
2024-01-08 01:42:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
 const fs = require('fs');
 const crypto = require('crypto');
 const path = require('path');
-
-const folderPath = 'files';
+const AdmZip = require("adm-zip");
 
 function calculateMD5(filePath) {
   const fileContent = fs.readFileSync(filePath);
   return crypto.createHash('md5').update(fileContent).digest('hex');
 }
 
-function generateFileHashes() {
+function generateFileHashes(folder) {
   const fileHashes = {};
 
-  // Read files from the specified folder
-  const files = fs.readdirSync(folderPath);
+  const files = fs.readdirSync(folder);
 
-  // Generate hashes for each file
   files.forEach((file) => {
-    if (file.endsWith('hash.json'))
+    if (file.endsWith('manifest.json'))
         return;
-    const filePath = path.join(folderPath, file);
+
+    const filePath = path.join(folder, file);
     const hash = calculateMD5(filePath);
-    fileHashes[file] = hash;
+    fileHashes[file] = {
+      path: filePath.replace(/\\/g, "/"),
+      hash: hash,
+      isZip: file.endsWith('.zip'),
+      isLauncherPCK: file.endsWith('.pck'),
+    };
+	
+    if (file.endsWith('.zip')) {
+      fileHashes[file].entries = [];
+
+      const zip = new AdmZip(filePath);
+
+      zip.getEntries().forEach((entry) => {
+        if (entry.isDirectory) {
+          return;
+        }
+
+        const entryHash = crypto.createHash('md5').update(entry.getData()).digest('hex');
+        
+        fileHashes[file].entries.push({
+          path: entry.entryName.replace(/\\/g, "/"),
+          hash: entryHash
+        })
+      })
+    }
   });
 
-  // Write the hashes to a JSON file
-  const outputFilePath = 'files/hash.json';
-  fs.writeFileSync(outputFilePath, JSON.stringify(fileHashes, null, 2));
+  const outputFilePath = 'files/manifest.json';
+  fs.writeFileSync(outputFilePath, JSON.stringify(fileHashes));
 
   console.log(`File hashes generated and saved to ${outputFilePath}`);
 }
 
-generateFileHashes();
+generateFileHashes('files');
